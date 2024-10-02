1ae4629f9a927b7704180eae114d6bc297c6522c
Cameorn Gallarno <cgallarno@gmail.com>
2024-10-02 18:29:50 -0400

    Cleanup, added some comments

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ git clone https://github.com/cgallarno/TranscriptAnalyzer.git
 cd TranscriptAnalyzer
 ```
 
-2. Create / activate virtual envrionment
+2. Create / activate virtual envrionment (optional)
 
 ```
 python3 -m venv venv
@@ -25,13 +25,22 @@ pip install -r requirements.txt
 ```
 
 4. Use it!
-   - Videos in common:
+
+#### Videos in common:
+
+##### Minecraft (lots of identical matches)
+
+```bash
+python3 main.py gam8OM_7Wn8 VPZlLr3hkd4
+```
+
+##### Among us
 
 ```bash
 python3 main.py VeH1fynKu6w 6KxT-yGbvBo
 ```
 
-    * Videos NOT in common:
+#### Videos NOT in common:
 
 ```bash
 python3 main.py VeH1fynKu6w 5pUCF6RQa50
--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def find_common_chunks(transcripts):
             transcript_1, transcript_2 = transcript_2, transcript_1
             video_id_1, video_id_2 = video_id_2, video_id_1
 
-        print(f"Determine video_id {video_id_1} is shorter")
+        print(f"Determined video_id {video_id_1} is shorter")
 
         matched_chunks = []
         for chunk_1 in transcript_1:
@@ -44,15 +44,18 @@ def find_common_chunks(transcripts):
             for chunk_2 in transcript_2:
                 if ' ' not in chunk_2['text']:
                     continue
+
                 # Using levenshtein distance to account for minor variances in YouTubes automatic captioning
+                # This is an expensive operation inside of doubly nested loop.
+                # Consider other algorithims such as LCS or n-gram, and hashing to avoid multiple loops    
                 similarity = normalize_levenshtein_distance(chunk_1['text'], chunk_2['text'])
                 if similarity > 0.6: 
                     chunk_1['video_id'] = video_id_1
                     chunk_2['video_id'] = video_id_2
+                    # TODO: Refactor into a MatchResult class
                     yield (chunk_1, chunk_2, f"{similarity:.2f}")
-                    #matched_chunks.append((chunk_1, chunk_2, f"{similarity:.2f}"))
 
-        #return matched_chunks
+        return matched_chunks
     except Exception as e:
         print(f"Error finding common chunks: {e}")
         return []
@@ -70,17 +73,13 @@ if __name__ == '__main__':
 
     matched_chunks = 0
     for chunk in find_common_chunks(transcripts):
-        matched_chunks = matched_chunks + 1
+        matched_chunks += 1
         print(f'===== Similarity {chunk[2]} ======')
         print(f"({chunk[0]['start']}) {chunk[0]['text']} - https://www.youtube.com/watch?v={chunk[0]['video_id']}&t={chunk[0]['start']}s")
         print(f"({chunk[1]['start']}) {chunk[1]['text']} - https://www.youtube.com/watch?v={chunk[1]['video_id']}&t={chunk[1]['start']}s")
-        print("===================================")
-        
-    
+        print("===================================") 
 
     print(f"Matched Chunks: {matched_chunks}")
-
-    # TODO: Normalize common chunks against total chunks
     print(f"Similarity across entire video: {matched_chunks / min(len(transcripts[args[0]]), len(transcripts[args[1]]))}")
 
    # print(json.dumps(common_chunks))
